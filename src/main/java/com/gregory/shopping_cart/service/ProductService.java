e21b72eda69bc27d63cd26e6a4b095e34c1c8ffe
Gregory Soares <gregory16704@gmail.com>
2025-06-11 16:57:20 -0300

    feat(create-product): adding a function to create products on every package

--- src/main/java/com/gregory/shopping_cart/service/ProductService.java
+++ src/main/java/com/gregory/shopping_cart/service/ProductService.java
@@ -1,5 +1,14 @@
 package com.gregory.shopping_cart.service;
 
+import com.gregory.shopping_cart.dao.AbstractDao;
+import com.gregory.shopping_cart.dao.ProductDao;
+import com.gregory.shopping_cart.model.entities.Product;
+
 public class ProductService {
 
+	AbstractDao<Product> dao = new ProductDao();
+	
+	public void create(Product product) {
+		dao.create(product);
+	}
 }
