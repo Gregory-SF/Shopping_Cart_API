9fb405be51cf3e22c3bb3f8b75f9b53236052432
Gregory Soares <gregory16704@gmail.com>
2025-06-16 14:16:30 -0300

    refactor(product): changed exceptions treatment and added a catch if one tries to delete a product saved on stock

--- src/main/java/com/gregory/shopping_cart/dao/StockDao.java
+++ src/main/java/com/gregory/shopping_cart/dao/StockDao.java
@@ -7,6 +7,4 @@ public class StockDao implements AbstractDao<Stock>{
 	public Stock findByProductId(Long id) {
 		return em.createQuery("FROM Stock WHERE product_id = :productId", Stock.class).setParameter("productId", id).getSingleResult();
 	}
-	
-	// Daria pra adicionar um find by categoria
 }
--- src/main/java/com/gregory/shopping_cart/service/ProductService.java
+++ src/main/java/com/gregory/shopping_cart/service/ProductService.java
@@ -3,8 +3,11 @@ package com.gregory.shopping_cart.service;
 import java.util.List;
 
 import javax.persistence.NoResultException;
+import javax.persistence.RollbackException;
 
 import com.gregory.shopping_cart.dao.ProductDao;
+import com.gregory.shopping_cart.dao.StockDao;
+import com.gregory.shopping_cart.exception.DbException;
 import com.gregory.shopping_cart.exception.InvalidValuesException;
 import com.gregory.shopping_cart.exception.NoProductException;
 import com.gregory.shopping_cart.model.entities.Product;
@@ -12,13 +15,15 @@ import com.gregory.shopping_cart.model.entities.Product;
 public class ProductService {
 
 	ProductDao dao = new ProductDao();
+	StockDao stockDao= new StockDao();
 
 	public void create(Product product) {
 		try {
 			productIsValid(product);
 			findByAttributes(product);
+			System.out.println("Product already registered");
 		} catch (NoProductException e) {
-			dao.create(product);				
+			dao.create(product);
 		}
 	}
 
@@ -34,39 +39,41 @@ public class ProductService {
 			dbProduct.setType(newProduct.getType());
 			dbProduct.setUnitValue(newProduct.getUnitValue());
 			dao.update(dbProduct);
-		} catch (NoResultException e){
+		} catch (NoResultException e) {
 			throw new NoProductException(e.getMessage());
-		}	
+		}
 	}
 
 	public Product findById(Long id) {
 		Product foundProduct = dao.findById(Product.class, id);
-		if(foundProduct != null) {
+		if (foundProduct != null) {
 			return foundProduct;
 		} else {
 			throw new NoProductException("Product not found!");
 		}
-
 	}
 
-	public void delete (Product product) {
+	public void delete(Product product) {
 		try {
 			dao.delete(findByAttributes(product));
-		} catch (NoResultException e){
+		} catch (NoProductException e) {
 			throw new NoProductException(e.getMessage());
+		} catch (RollbackException e) {
+			throw new DbException("This product is on stock, for safety measures, delete it from stock first!");
 		}
 	}
 
-	public Product findByAttributes(Product product) throws NoResultException{
+	public Product findByAttributes(Product product) throws NoResultException {
 		try {
 			return dao.findByAttributes(product);
 		} catch (NoResultException e) {
-			throw new NoResultException("Product doesn't exist! Operation canceled");
+			throw new NoProductException("Product doesn't exist! Operation canceled");
 		}
 	}
 
-	public void productIsValid(Product product) {
-		if(product.getUnitValue()<= 0 || product.getName()== "" || product.getType() == "") return;
-		throw new InvalidValuesException("Invalid values!");
+	private void productIsValid(Product product) {
+		if (product.getUnitValue() <= 0 || product.getName() == "" || product.getType() == "") {
+			throw new InvalidValuesException("Invalid values!");
+		}
 	}
 }
